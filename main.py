df001593bdeb580dd584039433e33f8a638875b1
rogel <rogeliohuitron45@gmail.com>
2024-02-13 16:18:32 -0600

    incomplete

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import pyperclip
 import time
 import pytesseract
-import selenium
-import webdriver_manager
 from PIL import ImageGrab
+from PIL import Image
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
-def display_text_in_browser(text):
-    driver = webdriver.Chrome()
-    driver.get("data:text/html,<h1 id=\"text\" style=\"text-align: center; margin-top: 50vh;\">"+text+"</h1>")
-    driver.quit()
+
+def display_text_in_browser(text, driver=None):
+    driver.get("")
+
+def display_hocr_in_browser(driver=None):
+    driver.get('file:///output.hocr')
 
 def main():
+    s = Service('C:\\tools\\geckodriver.exe')
+    option = Options()
+    option.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+    driver = webdriver.Firefox(option, s)
+
     while True:
         print("Press 'q' to quit")
         if input() == 'q':
+            if driver != None:
+                driver.quit()
             break
         else:
             screenshot = ImageGrab.grabclipboard()
             if screenshot:
-                text = pytesseract.image_to_string(screenshot)
-                print(text)
-                display_text_in_browser(text)
+                hocr_text = pytesseract.image_to_pdf_or_hocr(screenshot, extension='hocr')
+                with open('output.hocr', 'w+b') as f:
+                    f.write(hocr_text)
+                
+                ##text = pytesseract.image_to_string(screenshot)
+                ##print(text)
+                ##display_text_in_browser(text, driver)
+                display_hocr_in_browser(driver)
                 print("Text displayed in browser")
                 time.sleep(2)
             else:
                 print("No image found in clipboard")
-                time.sleep(2)
 
 if __name__ == "__main__":
     main()
